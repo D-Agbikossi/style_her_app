d6c0cc72ff34d5e2322a7a6e5ce98a5c98c2b899
Akhigbesimeon <s.akhigbe@alustudent.com>
2025-11-07 17:32:08 +0200

    Edited signup screen

--- frontend/lib/screens/signup_screen.dart
+++ frontend/lib/screens/signup_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/screens/login_screen.dart';
-import 'package:frontend/screens/interest_screen.dart';
 import 'package:frontend/screens/home_screen.dart';
 import '../main.dart';
 
