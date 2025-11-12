b28a959f0aa40b15d3d360ea5f28d31409121298
NFORMII <n.girbong@gmail.com>
2025-11-12 17:14:07 +0200

    done

---
+++ frontend/lib/widgets/badge_chip.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../theme.dart';
+
+class BadgeChip extends StatelessWidget {
+  final String label;
+  const BadgeChip(this.label, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+      decoration: BoxDecoration(
+        color: AppTheme.accent,
+        borderRadius: BorderRadius.circular(24),
+      ),
+      child: Text(label, style: const TextStyle(fontSize: 12, color: AppTheme.text)),
+    );
+  }
+}
